a7569d0958beece6433a4ce48261d14d5f055574
Allam <Allam.Tejaswi@bls.gov>
2017-05-10 15:34:14 -0400

    c

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const jsContainer = document.getElementById("js");
+const reactContainer = document.getElementById("react");
 
-jsContainer.innerHTML = '<div class="demo"> Hello JS </div>';
+jsContainer.innerHTML = '<div class="demo"> Hello JS <input/> </div>';
+
+// ReactDOM.render(
+//     React.createElement(
+//       "div",
+//       { className: "demo" },
+//       "Hello React"
+//     ),
+//     reactContainer
+//   );
