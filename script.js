ae1a940903ab7dea9bb1f08374aa30713338e786
Allam <Allam.Tejaswi@bls.gov>
2017-06-21 16:11:53 -0400

    Testing Date

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const jsContainer = document.getElementById("js");
 const reactContainer = document.getElementById("react");
 
-jsContainer.innerHTML = '<div class="demo"> Hello JS <input/> </div>';
+jsContainer.innerHTML = '<div class="demo"> Hello JS <input/> <p> ${new Date()} </p></div>';
 
 ReactDOM.render(
     React.createElement(
