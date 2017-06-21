f9c2d180055dc2dfe94120d72af0ad66637dcecd
Allam <Allam.Tejaswi@bls.gov>
2017-06-21 16:23:35 -0400

    Added setInterval

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-// import ReactDOM from 'react-dom'
-// import React from 'react'
+//  import ReactDOM from 'react-dom';
+//  import React from 'react';
 
 const jsContainer = document.getElementById("js");
 const reactContainer = document.getElementById("react");
 
-jsContainer.innerHTML = '<div class="demo"> Hello JS <input/> <p> ${new Date()} </p></div>';
+const render = () => {
+  jsContainer.innerHTML = `
+    <div class="demo">
+      Hello JS
+      <input />
+      <p>${new Date()}</p>
+    </div>
+  `;
 
-ReactDOM.render(
+  ReactDOM.render(
     React.createElement(
       "div",
       { className: "demo" },
-      "Hello React",
-      React.createElement("input")
+      "Hello React ",
+      React.createElement("input"),
+      React.createElement(
+        "p",
+        null,
+        new Date().toString()
+      )
     ),
     reactContainer
   );
+}
+
+setInterval(render, 1000);
