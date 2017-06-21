eaece4f6b7110476f3afa0cf16df6ff302eb1f2d
Allam <Allam.Tejaswi@bls.gov>
2017-06-21 16:08:46 -0400

    Not sure

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// import ReactDOM from 'react-dom'
+// import React from 'react'
+
 const jsContainer = document.getElementById("js");
 const reactContainer = document.getElementById("react");
 
 jsContainer.innerHTML = '<div class="demo"> Hello JS <input/> </div>';
 
-// ReactDOM.render(
-//     React.createElement(
-//       "div",
-//       { className: "demo" },
-//       "Hello React"
-//     ),
-//     reactContainer
-//   );
+ReactDOM.render(
+    React.createElement(
+      "div",
+      { className: "demo" },
+      "Hello React"
+    ),
+    reactContainer
+  );
